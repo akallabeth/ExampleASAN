7e00cfc81cf0d80f366be24d5cb0a50ce7448981
Armin Novak <armin.novak@thincast.com>
2020-06-25 11:32:38 +0200

    Fixed build.

--- app/src/main/java/com/gpxblog/exampleasan/MainActivity.java
+++ app/src/main/java/com/gpxblog/exampleasan/MainActivity.java
@@ -1,8 +1,12 @@
 package com.gpxblog.exampleasan;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.system.ErrnoException;
 import android.widget.TextView;
+
+import static android.system.Os.setenv;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,47 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
+        prepareAsan();
         System.loadLibrary("native-lib");
     }
+
+
+    private static void prepareAsan() {
+        final String[] abis = Build.SUPPORTED_ABIS;
+        for (String abi : abis) {
+            String arch;
+            if ("arm64-v8a".equals(abi)) {
+                arch = "aarch64";
+            } else if ("armeabi-v7a".equals(abi)) {
+                arch = "arm";
+            } else if ("armeabi".equals(abi)) {
+                arch = "arm";
+            } else if ("x86_64".equals(abi)) {
+                arch = "x86_64";
+            } else if ("x86".equals(abi)) {
+                arch = "i686";
+            } else {
+                throw new RuntimeException("Unsupported ABI " + abi);
+            }
+
+            final String asan = "clang_rt.asan-" + arch + "-android";
+            final String cpp = "c++_shared";
+
+            try {
+                System.loadLibrary(asan);
+                try {
+                    System.loadLibrary(cpp);
+                    setenv("LD_PRELOAD", asan + " " + cpp, true);
+                } catch(UnsatisfiedLinkError e) {
+                    setenv("LD_PRELOAD", asan, true);
+                }
+                setenv("ASAN_OPTIONS","log_to_syslog=false,allow_user_segv_handler=1", true);
+                return;
+            } catch (UnsatisfiedLinkError | ErrnoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
